696398e0e49048e560fb41cc7cef9cdf0f9e1a81
João Gabriel Piva <joaogpiva@gmail.com>
2023-04-25 20:26:57 -0300

    Commit inicial

---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+
+</script>
+
+<h1>Calculadora de Probabilidade Binomial</h1>
+
+<input placeholder="Digite o valor de p" />
+<input placeholder="Digite o valor de x" />
+<input placeholder="Digite o valor de n" />
+
+<button>Calcular</button>
