dfde9b5de2fca9e5c0858bfb7ff0fa6595db4093
Bernardo <bernardo.pretto@hub2b.com.br>
2023-04-25 20:41:31 -0300

    add handler

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,14 @@
-<script>
-
+<script lang="ts">
+    let p: number, n: number, x: number;
+    function handleClick () {
+        
+	}
 </script>
 
 <h1>Calculadora de Probabilidade Binomial</h1>
 
-<input placeholder="Digite o valor de p" />
-<input placeholder="Digite o valor de x" />
-<input placeholder="Digite o valor de n" />
+<input bind:value={p} placeholder="Digite o valor de p" />
+<input bind:value={n} placeholder="Digite o valor de n" />
+<input bind:value={x} placeholder="Digite o valor de x" />
 
-<button>Calcular</button>
+<button on:click={handleClick}>Calcular</button>
