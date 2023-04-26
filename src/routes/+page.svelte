f0f6dae3e458174db180bbb0ea990ccdabed73d8
João Gabriel Piva <joaogpiva@gmail.com>
2023-04-25 22:16:27 -0300

    Add reactive expression display

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,26 @@
 <script lang="ts">
-    let p: number, n: number, x: number;
+    import { display } from 'mathlifier';
+
+    let p: number, n: number, x: number, resultIndividual: number = 0, resultAcumulada: number = 0;
+    $: eq = display("P(x) = {" + (n ? n : 0) +  "\\choose " + (x ? x : 0) + "} \\cdot " 
+                    + (p ? p : 0) + "^{" + (x ? x : 0) + "} \\cdot " 
+                    + (p ? (1-p) : 1) + "^{" + (x && n ? (n - x) : 0) + "}");
     function handleClick () {
         
 	}
 </script>
 
-<h1>Calculadora de Probabilidade Binomial</h1>
+<div class="d-flex flex-column align-items-center justify-content-center p-2">
+    <h1>Calculadora de Probabilidade Binomial</h1>
 
-<input bind:value={p} placeholder="Digite o valor de p" />
-<input bind:value={n} placeholder="Digite o valor de n" />
-<input bind:value={x} placeholder="Digite o valor de x" />
+    <input bind:value={p} placeholder="Digite o valor de p" />
+    <input bind:value={n} placeholder="Digite o valor de n" />
+    <input bind:value={x} placeholder="Digite o valor de x" />
 
-<button on:click={handleClick}>Calcular</button>
+    <p>{@html eq}</p>
+    
+    <button on:click={handleClick}>Calcular</button>
+    
+    <h2>Resultado da probabilidade individual: { resultIndividual }</h2>
+    <h2>Resultado da probabilidade acumulada: { resultAcumulada }</h2>
+</div>
