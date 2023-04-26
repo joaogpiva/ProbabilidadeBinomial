b36c04f44bf27be1072823f49a2d6fc3d69179ae
Bernardo <bernardo.pretto@hub2b.com.br>
2023-04-25 21:39:10 -0300

    resultIndividual working

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,20 @@
 <script lang="ts">
-    let p: number, n: number, x: number;
-    function handleClick () {
-        
+    let p: number, n: number, x: number, resultIndividual: number, resultAcumulado: number;
+
+    function handleClick() {
+        let nFatorial = fatorial(n);
+        let xFatorial = fatorial(x);
+        let nSubxFatorial = fatorial(n-x);
+       
+        resultIndividual = nFatorial / xFatorial * nSubxFatorial * (p ** x) * ((1-p) ** (n - x));
 	}
+
+    function fatorial(numero: number): number {
+        if (numero < 0) {
+            return 1;
+        }
+        return numero === 0 ? 1 : numero * fatorial(numero - 1);
+    };
 </script>
 
 <h1>Calculadora de Probabilidade Binomial</h1>
@@ -12,3 +24,6 @@
 <input bind:value={x} placeholder="Digite o valor de x" />
 
 <button on:click={handleClick}>Calcular</button>
+
+<h3>Resultado individual: {resultIndividual}</h3>
+<h3>Resultado acumulado: {resultAcumulado}</h3>
