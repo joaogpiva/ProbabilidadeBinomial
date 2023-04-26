ce4be15dab05209ede9516e2471d25715973b7fc
Bernardo <bernardo.pretto@hub2b.com.br>
2023-04-25 22:31:07 -0300

    fix calculation, add inputs validation

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,27 +1,48 @@
 <script lang="ts">
-    let p: number, n: number, x: number, resultIndividual: number, resultAcumulado: number;
+    let p: number, n: number, x: number, resultIndividual: number = 0, resultAcumulado: number = 0;
 
     function handleClick() {
+        if (!validateInputs(p, n, x)){
+            //TODO
+            return;
+        }        
+
         let nFatorial = fatorial(n);
         let xFatorial = fatorial(x);
         let nSubxFatorial = fatorial(n-x);
-       
-        resultIndividual = nFatorial / xFatorial * nSubxFatorial * (p ** x) * ((1-p) ** (n - x));
+
+        resultIndividual = (nFatorial / (xFatorial * nSubxFatorial)) * (p ** x) * ((1-p) ** (n - x));
+        resultAcumulado = resultIndividual;
+
+        for (let index = 0; index <= x-1; index++) {
+            let indexFatorial = fatorial(index);
+            let nSubIndexFatorial = fatorial(n-index);
+            resultAcumulado += (nFatorial / (indexFatorial * nSubIndexFatorial)) * (p ** index) * ((1-p) ** (n - index));     
+        }
 	}
 
-    function fatorial(numero: number): number {
-        if (numero < 0) {
-            return 1;
+    function validateInputs(p: number, n: number, x: number): boolean {
+        if (!n || !x || !p) {
+            return false;
         }
+        if (x > n) {
+            return false;
+        }
+        return true;
+    }
+
+    function fatorial(numero: number): number {
+        if (numero < 0)
+            return 1;
         return numero === 0 ? 1 : numero * fatorial(numero - 1);
     };
 </script>
 
 <h1>Calculadora de Probabilidade Binomial</h1>
 
-<input bind:value={p} placeholder="Digite o valor de p" />
-<input bind:value={n} placeholder="Digite o valor de n" />
-<input bind:value={x} placeholder="Digite o valor de x" />
+<input bind:value={p} type="number" placeholder="Digite o valor de p" />
+<input bind:value={n} type="number" placeholder="Digite o valor de n" />
+<input bind:value={x} type="number" placeholder="Digite o valor de x" />
 
 <button on:click={handleClick}>Calcular</button>
 
