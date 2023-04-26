4e2735d0e7b48bca5c19fe486cf5adb4c181edb6
João Gabriel Piva <joaogpiva@gmail.com>
2023-04-26 15:27:08 -0300

    Add more reactive formulas, finish layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,26 +1,54 @@
 <script lang="ts">
-    import { display } from 'mathlifier';
+    import { math, display } from 'mathlifier';
 
     let p: number, n: number, x: number, resultIndividual: number = 0, resultAcumulada: number = 0;
     $: eq = display("P(x) = {" + (n ? n : 0) +  "\\choose " + (x ? x : 0) + "} \\cdot " 
                     + (p ? p : 0) + "^{" + (x ? x : 0) + "} \\cdot " 
-                    + (p ? (1-p) : 1) + "^{" + (x && n ? (n - x) : 0) + "}");
+                    + (p && p != 0 ? (1-p).toFixed(p.toString().split(".")[1].length) : 1) + "^{" + (x && n ? (n - x) : 0) + "}");
+
+    $: exprProbabilidadeIndividual = math("P(x = " + (x ? x : 0) + ")");
+    $: exprProbabilidadeAcumulada = math("P(x \\le " + (x ? x : 0) + ")");
+
     function handleClick () {
         
 	}
 </script>
 
-<div class="d-flex flex-column align-items-center justify-content-center p-2">
-    <h1>Calculadora de Probabilidade Binomial</h1>
+<div class="d-flex flex-column align-items-center justify-content-center min-vh-100 container-geral" >
+    <div class="p-3">
+        <h1>Calculadora de Probabilidade Binomial</h1>
+    </div>
 
-    <input bind:value={p} placeholder="Digite o valor de p" />
-    <input bind:value={n} placeholder="Digite o valor de n" />
-    <input bind:value={x} placeholder="Digite o valor de x" />
+    <div class="d-flex flex-column align-items-center justify-content-center p-3">
+        <form>
+            <div class="mb-2">
+                <input class="form-control" bind:value={p} placeholder="Digite o valor de p" />
+            </div>
+            <div class="mb-2">
+                <input class="form-control" bind:value={n} placeholder="Digite o valor de n" />
+            </div>
+            <div class="mb-2">
+                <input class="form-control" bind:value={x} placeholder="Digite o valor de x" />
+            </div>
+        </form> 
 
-    <p>{@html eq}</p>
-    
-    <button on:click={handleClick}>Calcular</button>
-    
-    <h2>Resultado da probabilidade individual: { resultIndividual }</h2>
-    <h2>Resultado da probabilidade acumulada: { resultAcumulada }</h2>
+        <p>{@html eq}</p>
+        <button class="btn btn-primary btn-calcular" on:click={handleClick}>Calcular</button>
+    </div>
+
+    <div class="p-3 d-flex flex-column align-items-center justify-content-center">
+        <h2>Resultado da probabilidade individual ({@html exprProbabilidadeIndividual}): { resultIndividual }</h2>
+        <h2>Resultado da probabilidade acumulada ({@html exprProbabilidadeAcumulada}): { resultAcumulada }</h2>
+    </div>
 </div>
+
+<style>
+.container-geral{
+    background-color: rgb(162, 255, 162);
+}
+
+.btn-calcular{
+    background-color: rgb(0,111,60);
+    border-color: #005930;
+}
+</style>
